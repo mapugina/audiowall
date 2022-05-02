dc2fa56db9491dae32f9a862665ca55d70c371d9
Michael Pugina <mapugina@gmail.com>
2022-05-01 22:53:01 -0600

    Adds attributions

--- apps/maestro/src/app/app.module.ts
+++ apps/maestro/src/app/app.module.ts
@@ -4,10 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { InstrumentComponent } from './instrument/instrument.component';
 import { OrchestraComponent } from './orchestra/orchestra.component';
+import { MaestrowallComponent } from './maestrowall/maestrowall.component';
+import { RouterModule } from '@angular/router';
+import { AttributionsComponent } from './attributions/attributions.component';
 
 @NgModule({
-  declarations: [AppComponent, InstrumentComponent, OrchestraComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    InstrumentComponent,
+    OrchestraComponent,
+    MaestrowallComponent,
+    AttributionsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'wall',
+      },
+      {
+        path: 'wall',
+        component: MaestrowallComponent,
+      },
+      {
+        path: 'attributions',
+        component: AttributionsComponent
+      },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ apps/maestro/src/app/maestrowall/maestrowall.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'audiowall-maestrowall',
+  templateUrl: './maestrowall.component.html',
+  styleUrls: ['./maestrowall.component.scss'],
+})
+export class MaestrowallComponent {
+}
